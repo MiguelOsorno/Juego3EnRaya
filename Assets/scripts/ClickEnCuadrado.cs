70e9d0a060435c1d1c5e1d2af3eda2aa2bee2a2e
Miguel Angel Osorno <miguel.osornolinares@gmail.com>
2019-03-15 19:12:09 -0600

    se desarrollo la funcionalidad principal del juego

---
+++ Assets/scripts/ClickEnCuadrado.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickEnCuadrado : MonoBehaviour {
+    public bool primerClick;
+    public bool repeticion;
+    public GameObject generadorIconos;
+    public ColocarIcono scripGeneradorIconos;
+    private float posicionX;
+    private float posicionY;
+	// Use this for initialization
+	void Start () {
+        scripGeneradorIconos = generadorIconos.gameObject.GetComponent<ColocarIcono>();
+	}
+
+    private void OnMouseDown()
+    {
+        primerClick = true;
+        if (primerClick && repeticion == false)
+        {
+            scripGeneradorIconos.crearIcono();
+            optenerCordenadas();
+            repeticion = true;
+        }
+    }
+
+
+
+    public void optenerCordenadas()
+    {
+        posicionX = this.gameObject.transform.position.x;
+        posicionY = this.gameObject.transform.position.y;
+        scripGeneradorIconos.posicionarIcono(posicionX, posicionY);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
---
+++ Assets/scripts/ColocarIcono.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColocarIcono : MonoBehaviour {
+    public GameObject tachaGo;
+    public GameObject circuloGo;
+    public GameObject[] iconos = new GameObject[9];
+    public int contador=0;
+    public Vector2 poscionNueva;
+    public bool turnoTacha;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void crearIcono()
+    {
+       
+        if(turnoTacha)
+        {
+            if(contador<iconos.Length)
+            {
+                iconos[contador] = Instantiate(tachaGo);
+                turnoTacha = false;
+            }
+        }
+        else
+        {
+            if (contador < iconos.Length)
+            {
+                iconos[contador] = Instantiate(circuloGo);
+                turnoTacha = true;
+            }
+        }
+    }
+
+    public void posicionarIcono(float x, float y)
+    {
+        poscionNueva = new Vector2(x, y);
+        iconos[contador].transform.position = poscionNueva;
+        ++contador;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
